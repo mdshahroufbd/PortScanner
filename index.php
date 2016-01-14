b857d177871f106e98146463f65b3c19b7501ae1
Filipe Laíns <filipe.lains@gmail.com>
2016-01-14 21:11:39 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
 <?php
+
+function scan($host = "google.com", $port = array(80)){
+    $r = array("open" => array(), "close" => array());
+    
+    foreach ($port as $p){
+        $con = @fsockopen($host, $p);
+        if(is_resource($con)){
+            array_push($r["open"], $p);
+            fclose($con);
+        } else {
+            array_push($r["close"], $p);
+        }
+    }
+}
+
+function convertRange($start = 0, $end = 0) {
+    
+}
+?>
